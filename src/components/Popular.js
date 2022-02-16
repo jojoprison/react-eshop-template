3239ac9f551ee47d6723bc58a446325ba8b70b74
Egor Zubachenko [zenbook] <egyabig2@gmail.com>
2022-02-17 02:27:33 +0300

    site parts update

---
+++ src/components/Popular.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => {
+
+    return (
+        <section className="section-name padding-y-sm">
+            <div className="container">
+                <header className="section-heading">
+                    <a href="#" className="btn btn-outline-primary float-right">See all</a>
+                    <h3 className="section-title">Popular products</h3>
+                </header>
+                </div>
+            </section>
+);
+};
+
+export default Nav;
---
+++ src/components/Products.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+const Products = () => {
+
+    return (
+
+<div class="row">
+    <div class="col-md-3">
+        <div href="#" class="card card-product-grid">
+            <a href="#" class="img-wrap"> <img src="assets/images/items/1.jpg" /> </a>
+            <figcaption class="info-wrap">
+                <a href="#" class="title">Just another product name</a>
+                <div class="price mt-1">$179.00</div>
+            </figcaption>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div href="#" class="card card-product-grid">
+            <a href="#" class="img-wrap"> <img src="assets/images/items/2.jpg" /> </a>
+            <figcaption class="info-wrap">
+                <a href="#" class="title">Some item name here</a>
+                <div class="price mt-1">$280.00</div>
+            </figcaption>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div href="#" class="card card-product-grid">
+            <a href="#" class="img-wrap"> <img src="assets/images/items/3.jpg" /> </a>
+            <figcaption class="info-wrap">
+                <a href="#" class="title">Great product name here</a>
+                <div class="price mt-1">$56.00</div>
+            </figcaption>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div href="#" class="card card-product-grid">
+            <a href="#" class="img-wrap"> <img src="assets/images/items/4.jpg" /> </a>
+            <figcaption class="info-wrap">
+                <a href="#" class="title">Just another product name</a>
+                <div class="price mt-1">$179.00</div>
+            </figcaption>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div href="#" class="card card-product-grid">
+            <a href="#" class="img-wrap"> <img src="assets/images/items/5.jpg" /> </a>
+            <figcaption class="info-wrap">
+                <a href="#" class="title">Just another product name</a>
+                <div class="price mt-1">$179.00</div>
+            </figcaption>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div href="#" class="card card-product-grid">
+            <a href="#" class="img-wrap"> <img src="assets/images/items/6.jpg" /> </a>
+            <figcaption class="info-wrap">
+                <a href="#" class="title">Some item name here</a>
+                <div class="price mt-1">$280.00</div>
+            </figcaption>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div href="#" class="card card-product-grid">
+            <a href="#" class="img-wrap"> <img src="assets/images/items/7.jpg" /> </a>
+            <figcaption class="info-wrap">
+                <a href="#" class="title">Great product name here</a>
+                <div class="price mt-1">$56.00</div>
+            </figcaption>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div href="#" class="card card-product-grid">
+            <a href="#" class="img-wrap"> <img src="assets/images/items/9.jpg" /> </a>
+            <figcaption class="info-wrap">
+                <a href="#" class="title">Just another product name</a>
+                <div class="price mt-1">$179.00</div>
+            </figcaption>
+        </div>
+    </div>
+</div>
+);
+};
+export default Products;
