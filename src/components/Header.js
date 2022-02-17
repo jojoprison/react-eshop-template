1dd59461454a25b04cf821729ebbc98e03f71c98
Egor Zubachenko [zenbook] <egyabig2@gmail.com>
2022-02-18 00:38:34 +0300

    new change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Header = () => {
 
 
@@ -7,15 +8,16 @@ const Header = () => {
         <div className="App">
     <header className="section-header">
         <section className="header-main border-bottom">
-            <div className="container">
+            <div className="container containerMain">
                 <div className="row align-items-center">
-                    <div className="col-lg-2 col-4">
-                        ООО «НПО НКС групп»
-                    </div>
-                    <div className="col-lg-6 col-sm-12">
+                        <a className="navbar-brand nav-item" href="#">
+                            <img src='assets/images/logos/logo.png' width="60" height="60"
+    className="d-inline-block align-center " alt=""/>
+                              ООО «НПО НКС групп» </a>
+                    <div className="col-lg-3 col-sm-9">
                         <form action="#" className="search">
                             <div className="input-group w-100">
-                                <input type="text" className="form-control" placeholder="Search"/>
+                                <input id="search-header" type="text" className="form-control" placeholder="Search"/>
                                 <div className="input-group-append">
                                     <button className="btn btn-primary" type="submit">
                                         <i className="fa fa-search"></i>
@@ -24,8 +26,9 @@ const Header = () => {
                             </div>
                         </form>
                     </div>
-                    <div className="col-lg-4 col-sm-6 col-12">
-                        <div className="widgets-wrap float-md-right">
+                        <a className="nav-link contact" href="#">Телефон</a>
+                <div className="col-lg-4 col-sm-6 col-12">
+                        <div className="widgets-wrap float-md-right rightside">
                             <div className="widget-header  mr-3">
                                 <a href="#" className="icon icon-sm rounded-circle border"><i
                                     className="fa fa-shopping-cart"></i></a>
@@ -35,7 +38,7 @@ const Header = () => {
                                 <a href="#" className="icon icon-sm rounded-circle border"><i
                                     className="fa fa-user"></i></a>
                                 <div className="text">
-                                    <span className="text-muted">Welcome!</span>
+                                    <span className="text-black-50">Welcome!</span>
                                     <div>
                                         <a href="#">Sign in</a> |
                                         <a href="#"> Register</a>
--- src/components/Popular.js
+++ src/components/Popular.js
@@ -5,10 +5,10 @@ const Nav = () => {
     return (
         <section className="section-name padding-y-sm">
             <div className="container">
-                <header className="section-heading">
+                <header-second className="section-heading">
                     <a href="#" className="btn btn-outline-primary float-right">See all</a>
                     <h3 className="section-title">Popular products</h3>
-                </header>
+                </header-second>
                 </div>
             </section>
 );
