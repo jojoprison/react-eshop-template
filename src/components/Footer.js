21e8f62ff0faba46bdc46c03acd9359f6676598a
Egor [home] Zubachenko <egyabig2@gmail.com>
2022-02-18 05:26:36 +0300

    refactoring

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,17 @@ import React from "react";
 const Footer = () => {
 
     return (
-
         <footer className="section-footer border-top bg">
             <div className="container">
                 <section className="footer-top  padding-y">
                     <div style={{position: 'relative'}} className="row">
-                        {/*//*/}
+
                         <a id='logo_company_footer' href="/">
                             <img src='assets/images/logos/logo.png' width="60" height="60"
                                  className="d-inline-block align-center col-3" alt="logo"/>
                             <span style={{fontWeight: "600"}}> ООО «НПО НКС групп» </span>
                         </a>
-                        {/*//*/}
+
                         <aside className="col-md col-ld-4 col-12">
                             <h6 className="title">Компания</h6>
                             <ul className="list-unstyled col-12">
@@ -23,6 +22,7 @@ const Footer = () => {
                                 <li><a href="#"> <u>Каталог</u> </a></li>
                             </ul>
                         </aside>
+
                         <aside className="col-md col-ld-4 col-12">
                             <h6 className="title">Профиль</h6>
                             <ul className="list-unstyled">
@@ -31,12 +31,11 @@ const Footer = () => {
                                 <li><a href="#"> <u>Моя корзина</u> </a></li>
                             </ul>
                         </aside>
+
                     </div>
                 </section>
             </div>
         </footer>
-
-
     );
 };
 export default Footer;
--- src/components/Products.js
+++ src/components/Products.js
@@ -3,82 +3,91 @@ import React from "react";
 const Products = () => {
 
     return (
-<div className="container">
-<div class="row">
-    <div class="col-md-3">
-        <div href="#" class="card card-product-grid">
-            <a href="#" class="img-wrap"> <img src="assets/images/items/1.jpg" /> </a>
-            <figcaption class="info-wrap">
-                <a href="#" class="title">Just another product name</a>
-                <div class="price mt-1">$179.00</div>
-            </figcaption>
+        <div className="container">
+            <div className="row">
+
+                <div className="col-md-3">
+                    <div href="#" className="card card-product-grid">
+                        <a href="#" className="img-wrap"> <img src="assets/images/items/1.jpg"/> </a>
+                        <figcaption className="info-wrap">
+                            <a href="#" className="title">Just another product name</a>
+                            <div className="price mt-1">$179.00</div>
+                        </figcaption>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div href="#" className="card card-product-grid">
+                        <a href="#" className="img-wrap"> <img src="assets/images/items/2.jpg"/> </a>
+                        <figcaption className="info-wrap">
+                            <a href="#" className="title">Some item name here</a>
+                            <div className="price mt-1">$280.00</div>
+                        </figcaption>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div href="#" className="card card-product-grid">
+                        <a href="#" className="img-wrap"> <img src="assets/images/items/3.jpg"/> </a>
+                        <figcaption className="info-wrap">
+                            <a href="#" className="title">Great product name here</a>
+                            <div className="price mt-1">$56.00</div>
+                        </figcaption>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div href="#" className="card card-product-grid">
+                        <a href="#" className="img-wrap"> <img src="assets/images/items/4.jpg"/> </a>
+                        <figcaption className="info-wrap">
+                            <a href="#" className="title">Just another product name</a>
+                            <div className="price mt-1">$179.00</div>
+                        </figcaption>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div href="#" className="card card-product-grid">
+                        <a href="#" className="img-wrap"> <img src="assets/images/items/5.jpg"/> </a>
+                        <figcaption className="info-wrap">
+                            <a href="#" className="title">Just another product name</a>
+                            <div className="price mt-1">$179.00</div>
+                        </figcaption>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div href="#" className="card card-product-grid">
+                        <a href="#" className="img-wrap"> <img src="assets/images/items/6.jpg"/> </a>
+                        <figcaption className="info-wrap">
+                            <a href="#" className="title">Some item name here</a>
+                            <div className="price mt-1">$280.00</div>
+                        </figcaption>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div href="#" className="card card-product-grid">
+                        <a href="#" className="img-wrap"> <img src="assets/images/items/7.jpg"/> </a>
+                        <figcaption className="info-wrap">
+                            <a href="#" className="title">Great product name here</a>
+                            <div className="price mt-1">$56.00</div>
+                        </figcaption>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div href="#" className="card card-product-grid">
+                        <a href="#" className="img-wrap"> <img src="assets/images/items/9.jpg"/> </a>
+                        <figcaption className="info-wrap">
+                            <a href="#" className="title">Just another product name</a>
+                            <div className="price mt-1">$179.00</div>
+                        </figcaption>
+                    </div>
+                </div>
+            </div>
+
         </div>
-    </div>
-    <div class="col-md-3">
-        <div href="#" class="card card-product-grid">
-            <a href="#" class="img-wrap"> <img src="assets/images/items/2.jpg" /> </a>
-            <figcaption class="info-wrap">
-                <a href="#" class="title">Some item name here</a>
-                <div class="price mt-1">$280.00</div>
-            </figcaption>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div href="#" class="card card-product-grid">
-            <a href="#" class="img-wrap"> <img src="assets/images/items/3.jpg" /> </a>
-            <figcaption class="info-wrap">
-                <a href="#" class="title">Great product name here</a>
-                <div class="price mt-1">$56.00</div>
-            </figcaption>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div href="#" class="card card-product-grid">
-            <a href="#" class="img-wrap"> <img src="assets/images/items/4.jpg" /> </a>
-            <figcaption class="info-wrap">
-                <a href="#" class="title">Just another product name</a>
-                <div class="price mt-1">$179.00</div>
-            </figcaption>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div href="#" class="card card-product-grid">
-            <a href="#" class="img-wrap"> <img src="assets/images/items/5.jpg" /> </a>
-            <figcaption class="info-wrap">
-                <a href="#" class="title">Just another product name</a>
-                <div class="price mt-1">$179.00</div>
-            </figcaption>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div href="#" class="card card-product-grid">
-            <a href="#" class="img-wrap"> <img src="assets/images/items/6.jpg" /> </a>
-            <figcaption class="info-wrap">
-                <a href="#" class="title">Some item name here</a>
-                <div class="price mt-1">$280.00</div>
-            </figcaption>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div href="#" class="card card-product-grid">
-            <a href="#" class="img-wrap"> <img src="assets/images/items/7.jpg" /> </a>
-            <figcaption class="info-wrap">
-                <a href="#" class="title">Great product name here</a>
-                <div class="price mt-1">$56.00</div>
-            </figcaption>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div href="#" class="card card-product-grid">
-            <a href="#" class="img-wrap"> <img src="assets/images/items/9.jpg" /> </a>
-            <figcaption class="info-wrap">
-                <a href="#" class="title">Just another product name</a>
-                <div class="price mt-1">$179.00</div>
-            </figcaption>
-        </div>
-    </div>
-</div>
-</div>
-);
+    );
 };
 export default Products;
