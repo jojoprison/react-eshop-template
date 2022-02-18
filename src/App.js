03d60c7a20021983333b5a091ecc115d60708f5b
Egor [home] Zubachenko <egyabig2@gmail.com>
2022-02-18 14:11:31 +0300

    css -> src, import from index, routing for BACK-END
    перенес часть css файлов в src/css, чтобы вызывать import <file> прямо из реакта, а то стили не грузились
    поставил пару либ, добавил метод для получения данных из бд

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import './App.css';
-
 import React from "react";
-import {Header} from "./components/Header";
-import {Navigation} from './components/Nav'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navigation from './components/Nav'
+import {Table} from "./components/Table";
+import {Home} from "./pages/Home";
+import {Header} from "./pages/Header";
+import {Footer} from "./pages/Footer";
+import './App.css';
 
 function App() {
     return (
-        <div className="container">
-            <Header/>
-            <Navigation/>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <Navigation/>
+
+                <Routes>
+                    {/*component={Home} dont work!*/}
+                    <Route path="/" element={<Home/>} exact/>
+                    <Route path="/table" element={<Table/>}/>
+                </Routes>
+
+                <Footer/>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import './css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './css/responsive.css';
+import './css/styles.css';
+// TODO мы изначально писали на 4.3 версии бустрапа, этот - 5.1; из-за чего не так что-то отрисовывается
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import 'jquery';
+import 'popper.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   <React.StrictMode>
