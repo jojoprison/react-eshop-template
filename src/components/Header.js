e7bf3e719c2b81f07d9e57f7b2fb0f636d7a2239
Egor [home] Zubachenko <egyabig2@gmail.com>
2022-02-18 05:26:57 +0300

    responsive header (exclude mobile)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,66 +3,66 @@ import React from "react";
 
 const Header = () => {
 
-
     return (
-        <div className="App">
-    <header className="section-header">
-        <section className="header-main border-bottom">
-            <div className="container">
-                <div className="row no-gutters align-items-center">
-                    <div className="col-xxl-3 col-lg-4 col-md-2 col-sm-2 col-12">
-                        {/*TODO мб убрать navbar-brand*/}
-                        <a  href="/">
-                            <img src='assets/images/logos/logo.png' width="60" height="60"
-                                 className="d-inline-block align-center " alt=""/>
-                          <span style={{fontWeight: "600"}}> ООО «НПО НКС групп» </span>
-                        </a>
-                    </div>
+        <header className="section-header">
+            <section className="header-main border-bottom">
+                <div className="container">
+                    <div className="row align-items-baseline">
 
+                        <div
+                            className="col-lg-3 col-md-6 p-lg-0 pt-md-2">
+                            <a href="/">
+                                <img src='assets/images/logos/logo.png' width="60" height="60"
+                                     className="logo" alt=""/>
+                                ООО «НПО НКС групп»
+                            </a>
+                        </div>
 
-                    <div id="search-header-div" className="col-xxl-5 col-xl-5 col-lg-3 col-md-2 col-sm-1">
-                        <form action="#" className="search">
-                            <div className="input-group w-100">
-                                <input id="search-header" type="text" className="form-control" placeholder="Search"/>
-                                <div className="input-group-append">
-                                    <button className="btn nks-btn" type="submit">
-                                        <i className="fa fa-search"></i>
-                                    </button>
+                        <div id="search-header-div"
+                             className="col-xxl-5 col-xl-5 col-lg-5 col-md-auto order-lg-1 order-md-last">
+                            <form action="#" className="search">
+                                <div className="input-group w-100">
+                                    <input id="search-header" type="text" className="form-control"
+                                           placeholder="Search"/>
+                                    <div className="input-group-append">
+                                        <button className="btn nks-btn" type="submit">
+                                            <i className="fa fa-search"/>
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
-                    </div>
+                            </form>
+                        </div>
 
-                    <div className="col-xxl-1 col-xl-1 col-lg-1 col-md-2 col-sm-2 col-12">
-                        <a className="nav-link contact" href="#">Телефон</a>
-                    </div>
+                        <div className="col-lg-1 col-md order-md-1 p-lg-0">
+                            <a className="nav-link contact" href="#">Телефон</a>
+                        </div>
 
-
-                <div className="col-xxl-3 col-lg-4 col-md-4 col-sm-2 col-12">
-                        <div className="widgets-wrap float-md-right rightside">
-                            <div className="widget-header  mr-3">
-                                <a href="#" className="icon icon-sm rounded-circle border"><i
-                                    className="fa fa-shopping-cart"></i></a>
-                                <span className="badge badge-pill badge-danger notify">0</span>
-                            </div>
-                            <div className="widget-header icontext">
-                                <a href="#" className="icon icon-sm rounded-circle border"><i
-                                    className="fa fa-user"></i></a>
-                                <div className="text">
-                                    <span className="text-black-50">Welcome!</span>
-                                    <div>
-                                        <a href="#">Sign in</a> |
-                                        <a href="#"> Register</a>
+                        <div id="widgets_header"
+                             className="col-xl-3 col-lg col-md order-md-2 align-self-start p-lg-0 mb-md-3">
+                            <div className="widgets-wrap float-lg-right">
+                                <div className="widget-header mr-3">
+                                    <a href="#" className="icon icon-sm rounded-circle border">
+                                        <i className="fa fa-shopping-cart"></i></a>
+                                    <span className="badge badge-pill badge-danger notify">0</span>
+                                </div>
+                                <div className="widget-header icontext">
+                                    <a href="#" className="icon icon-sm rounded-circle border">
+                                        <i className="fa fa-user"></i></a>
+                                    <div className="text mobile-hide">
+                                        <span className="text-black-50">Welcome!</span>
+                                        <div>
+                                            <a href="#">Sign in</a> |
+                                            <a href="#"> Register</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
-            </div>
-        </section>
-    </header>
-</div>
+            </section>
+        </header>
     );
 };
 
