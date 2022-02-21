973b48f93ea178b540bae518373fc54d4cf6b94e
Egor [home] Zubachenko <egyabig2@gmail.com>
2022-02-21 11:20:05 +0300

    react nav init

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import {Navbar, Nav} from "react-bootstrap";
+import {Navbar, Nav, Container, NavDropdown, Dropdown, Button} from "react-bootstrap";
 
 export default function Navigation() {
 
     return (
-        // className="d-inline p-2 bg-dark text-white"
-        <Navbar collapseOnSelect variant="dark" bg="dark" expand="lg">
-            <div className="container">
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <NavLink to="/">Home</NavLink>
-                        <NavLink to="/table">Table</NavLink>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home"> НКС Групп</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <NavDropdown title="сча все ь=будет" trigger ="hover" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-            </div>
+            </Container>
         </Navbar>
+    )
+};
 
         // TODO закомментил, чтобы собрать динамический компонент реакта, через вторую ссыль получаю с сервака данные
 
@@ -74,5 +89,3 @@ export default function Navigation() {
         //         </div>
         //     </div>
         // </nav>
-    );
-}
