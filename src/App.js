cac78b98f997ab2455251d3371ba2c9448376be4
Egor [zenbook] Zubachenko <egyabig2@gmail.com>
2022-03-11 02:16:28 +0300

    меняем конпку возврата вверх НАХ

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Navigation from './components/Nav'
 import {Table} from "./components/Table";
@@ -7,12 +7,30 @@ import {Header} from "./pages/Header";
 import {Footer} from "./pages/Footer";
 import {Catalog} from "./components/Catalog";
 import {Product} from "./components/Product";
-import {Top} from "./pages/Top";
 import './App.css';
-import $ from 'jquery';
-import Popper from "popper.js";
 
 function App() {
+    const [showButton, setShowButton] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            // TODO вот тут эта ебатория проверяет на скок мы скрольнули экран И НЕ СКРЫВАЕТ КНОПКУ ОБРАТНО СУКА
+            // TODO мб пробнуть screenY хз
+            if (window.screenTop > 20) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        });
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
     return (
         <BrowserRouter>
             <div className="App">
@@ -26,7 +44,13 @@ function App() {
                     <Route path="/catalog" element={<Catalog/>}/>
                     <Route path="product/:id" element={<Product/>}/>
                 </Routes>
-                <Top/>
+
+                {showButton && (
+                    <button onClick={scrollToTop} className='back-to-top'>
+                        &#8679;
+                    </button>
+                )}
+
                 <Footer/>
             </div>
         </BrowserRouter>
