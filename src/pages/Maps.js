b7f89d12a3c5f03999fafb1ae1253de30a7412b8
Egor [zenbook] Zubachenko <egyabig2@gmail.com>
2022-03-14 00:27:38 +0300

    aOya

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -5,7 +5,7 @@ export const Maps = () => (
     <div className="container mb-5 pl-5 pr-5 map">
         <iframe
             src="https://yandex.ru/map-widget/v1/?um=constructor%3A3c7508f20a03d716f500eb108d0922c161e895d8ecda55e258772d8b7c9f3420&amp;source=constructor"
-            width="100%" height="475" frameBorder="30">
+            width="100%" height="500" frameBorder="30">
         </iframe>
     </div>
 )
