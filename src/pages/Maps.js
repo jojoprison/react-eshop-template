bf71746e41df70b313cf3ec16673b7a89f01d7e8
Egor [home] Zubachenko <egyabig2@gmail.com>
2022-03-13 20:12:29 +0300

    new maps

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -4,8 +4,8 @@ export const Maps = () => (
 
     <div className="container mb-5 pl-5 pr-5 map">
         <iframe
-            src="https://yandex.ru/map-widget/v1/?um=constructor%3A48ef50ed14566d93bc3881fdce02a80eb5f71b541e785117d50a747284033add&amp;source=constructor"
-            width="100%" height="400" frameBorder="30">
+            src="https://yandex.ru/map-widget/v1/?um=constructor%3A3c7508f20a03d716f500eb108d0922c161e895d8ecda55e258772d8b7c9f3420&amp;source=constructor"
+            width="100%" height="475" frameBorder="30">
         </iframe>
     </div>
 )
