fa4f0c0c7a3acbc86c3437ac86d674b43889ad05
Egor [home] Zubachenko <egyabig2@gmail.com>
2022-03-14 05:46:03 +0300

    нав переделан на реакт-бутстрап со ссылками

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,37 +1,31 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 import {Navbar, Nav, Container, NavDropdown, Dropdown, Button} from "react-bootstrap";
 
 export default function Navigation() {
 
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="blue" variant="dark">
             <Container>
-                {/*<NavLink to="/">Тестили Table</NavLink>*/}
-                <Navbar.Brand href="#home">Главная</Navbar.Brand>
+                <Navbar.Brand href="/">Главная</Navbar.Brand>
                 {/* TODO пробнуть aria-controls="basic-navbar-nav", пока хз в чем разница) */}
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        {/* TODO нужно как то реализовать NavLink вместо Nav.Link со стилями! */}
-                        {/*<NavLink to="/table">Тестили Table</NavLink>*/}
-                        {/*<NavLink to="/catalog">Каталог</NavLink>*/}
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="/table">Таблица</Nav.Link>
+                        <Nav.Link href="/catalog">Каталог</Nav.Link>
                         {/* TODO вот такой дропдаун - точно юзаем */}
-                        <NavDropdown title="сча все будет выпадать наХ" trigger ="hover" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown title="Столы" trigger ="hover" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Высокие</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Еще action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Еще</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Последние</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                    {/* третий способ размщения nav.link */}
                     <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
+                        <Nav.Link href="#deets">Шутка</Nav.Link>
+                        <Nav.Link eventKey={3} href="/filters">
+                            Еще разок фильтры
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
