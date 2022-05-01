2f7647adb3a3f72c13144380eced35a9bdaaf94e
jsliq_d <egyabig2@gmail.com>
2022-05-01 14:27:37 +0300

    add redirects

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {useCart} from "react-use-cart";
 import { ModalAuth } from "./auth/ModalAuth";
+import {Link} from "react-router-dom";
 
 const Header = (props) => {
     const {totalItems} = useCart();
@@ -11,13 +12,14 @@ const Header = (props) => {
             <section className="header-main header-height border-bottom">
 
                 {props.modal ?
+                    // TODO юзать другой компонент
                     <ModalAuth
                         authUser={props.authUser}
                         authToken={props.authToken}
                         setAuthToken={props.setAuthToken}
                         setModal={props.setModal}
                         modal={props.modal}
-                /> : <></>}
+                    /> : <></>}
 
                 <div className="container">
                     <div className="row align-items-baseline">
@@ -30,39 +32,45 @@ const Header = (props) => {
                             </a>
                         </div>
 
-                        <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-auto m-0 order-lg-1 order-md-last">
-                            <form action="#" className="search">
-                                <div className="input-group w-100">
-                                    <input id="search-header" type="text" className="form-control"
-                                           placeholder="Поиск..."/>
-                                    <div className="input-group-append">
-                                        <button className="btn btn-nks" type="submit">
-                                            <i className="fa fa-search"/>
-                                        </button>
+                        {/* если юзер не авторизирован */}
+                        {props.authToken === null ? (
+                            <button onClick={() => props.setModal(!props.modal)}>Войти</button>
+                        ) : (
+                            <div>
+                                <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-auto m-0 order-lg-1 order-md-last">
+                                    <form action="#" className="search">
+                                        <div className="input-group w-100">
+                                            <input id="search-header" type="text" className="form-control"
+                                                   placeholder="Поиск..."/>
+                                            <div className="input-group-append">
+                                                <button className="btn btn-nks" type="submit">
+                                                    <i className="fa fa-search"/>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                <div className="col-xl col-lg-1 col-md order-md-1 p-lg-0">
+                                    <a className="link-nks-light header-link" href="/contacts">Контакты</a>
+                                </div>
+
+                                <div className="col-xl-2 col-lg-2 col-md order-md-2 align-self-center p-lg-0 mb-md-3">
+                                    <div className="icon-link-badged mr-3">
+                                        <a href="/basket" className="icon-md">
+                                            <i className="fa fa-shopping-cart"/>
+                                        </a>
+                                        <span className="badge badge-pill badge-light notify">{totalItems}</span>
+                                    </div>
+
+                                    <div className="widget-header icontext">
+                                        <a href="/profile" className="icon icon-sm rounded-circle">
+                                            <i className="fa fa-user"/>
+                                        </a>
                                     </div>
                                 </div>
-                            </form>
-                        </div>
-
-                        <div className="col-xl col-lg-1 col-md order-md-1 p-lg-0">
-                            <a className="link-nks-light header-link" href="/contacts">Контакты</a>
-                        </div>
-
-                        <div className="col-xl-2 col-lg-2 col-md order-md-2 align-self-center p-lg-0 mb-md-3">
-                            <div className="icon-link-badged mr-3">
-                                <a href="/basket" className="icon-md">
-                                    <i className="fa fa-shopping-cart"/>
-                                </a>
-                                <span className="badge badge-pill badge-light notify">{totalItems}</span>
                             </div>
-
-                            <div className="widget-header icontext">
-                                <a href="#" className="icon icon-sm rounded-circle">
-                                    <i className="fa fa-user"/></a>
-                            </div>
-
-                        </div>
-
+                        )}
                     </div>
                 </div>
             </section>
